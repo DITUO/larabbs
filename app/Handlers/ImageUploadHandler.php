c1e814796abaf75f4011b9eb6c18ab2be6648e81
TuoDi <337361454@qq.com>
2018-07-05 18:44:25 +0800

    Avatar

---
+++ app/Handlers/ImageUploadHandler.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Handlers;
+
+class ImageUploadHandler{
+    protected $allowed_ext = ["png","jpg","gif","jpeg"];
+
+    public function save($file,$folder,$file_prefix){
+        $folder_name = "uploads/images/$folder/".date("Ym/d",time());
+
+        $upload_path = public_path().'/'.$folder_name;
+
+        $extension = strtolower($file->getClientOriginalExtension()) ? : 'png';
+
+        $filename = $file_prefix . '_' . time() . '_' . str_random(10) . '.' . $extension;
+
+        if(!in_array($extension,$this->allowed_ext)){
+            return false;
+        }
+
+        $file->move($upload_path,$filename);
+        $aaa = '/public'."/$folder_name/$filename";
+        $data = ['path' => $aaa];
+        return $data;
+    }
+}
