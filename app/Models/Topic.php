c2a6137bce1377344709681cea3025533130d2e9
TuoDi <337361454@qq.com>
2018-07-06 12:03:25 +0800

    Topics data

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -4,5 +4,9 @@ namespace App\Models;
 
 class Topic extends Model
 {
+    public $primaryKey='id';//主键
+
+    protected $table = 'topics';//表名
+
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
 }
--- database/seeds/TopicsTableSeeder.php
+++ database/seeds/TopicsTableSeeder.php
@@ -2,16 +2,26 @@
 
 use Illuminate\Database\Seeder;
 use App\Models\Topic;
+use App\Models\User;
+use App\Models\Category;
 
 class TopicsTableSeeder extends Seeder
 {
     public function run()
     {
-        $topics = factory(Topic::class)->times(50)->make()->each(function ($topic, $index) {
-            if ($index == 0) {
-                // $topic->field = 'value';
-            }
-        });
+        $user_ids = User::all()->pluck('id')->toArray();
+
+        $category_ids = Category::all()->pluck('id')->toArray();
+
+        $faker = app(Faker\Generator::class);
+
+        $topics = factory(Topic::class)
+                ->times(50)
+                ->make()
+                ->each(function ($topic, $index) use($user_ids, $category_ids, $faker) {
+                    $topic->user_id = $faker->randomElement($user_ids);
+                    $topic->category_id = $faker->randomElement($category_ids);
+                });
 
         Topic::insert($topics->toArray());
     }
