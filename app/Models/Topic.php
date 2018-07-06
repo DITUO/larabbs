39edb13e15122e778efc9c49f91838b7d5ce252a
TuoDi <337361454@qq.com>
2018-07-06 12:22:02 +0800

    Topic

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -9,4 +9,12 @@ class Topic extends Model
     protected $table = 'topics';//表名
 
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+    public function category(){
+        return $this->belongsTo(Category::class,'category_id','id');
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class,'user_id','id');
+    }
 }
