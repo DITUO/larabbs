ff9f0f53a3ce67658ed5a7dbafd6e23d2b8de8d8
TuoDi <337361454@qq.com>
2018-07-09 14:48:19 +0800

    User upload topic image

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
 use Auth;
+use App\Handlers\ImageUploadHandler;
 
 class TopicsController extends Controller
 {
@@ -61,5 +62,23 @@ class TopicsController extends Controller
 		$topic->delete();
 
 		return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
+	}
+
+	public function uploadImage(Request $request,ImageUploadHandler $uploader){
+		$data = [
+			'success' => false,
+			'msg' => '上传失败!',
+			'file_path' => ''
+		];
+
+		if($file = $request->upload_file){
+			$result = $uploader->save($request->upload_file,'topics',\Auth::id(),1024);
+			if($result){
+				$data['file_path'] = $result['path'];
+				$data['msg'] = '上传成功';
+				$data['success'] = true;
+			}
+		}
+		return $data;
 	}
 }
--- routes/web.php
+++ routes/web.php
@@ -34,5 +34,6 @@ Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
 Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
 
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
 
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
