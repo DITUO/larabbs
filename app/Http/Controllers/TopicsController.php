a04383b40e1eb8c17d9f03dfb2056eec1122d2d3
TuoDi <337361454@qq.com>
2018-07-09 15:02:43 +0800

    Topic detail page

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -39,7 +39,7 @@ class TopicsController extends Controller
 		$topic->fill($request->all());
 		$topic->user_id = Auth::id();
 		$topic->save();
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Created successfully.');
+		return redirect()->route('topics.show', $topic->id)->with('success', '创建成功.');
 	}
 
 	public function edit(Topic $topic)
@@ -53,7 +53,7 @@ class TopicsController extends Controller
 		$this->authorize('update', $topic);
 		$topic->update($request->all());
 
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Updated successfully.');
+		return redirect()->route('topics.show', $topic->id)->with('success', '更新成功.');
 	}
 
 	public function destroy(Topic $topic)
@@ -61,7 +61,7 @@ class TopicsController extends Controller
 		$this->authorize('destroy', $topic);
 		$topic->delete();
 
-		return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
+		return redirect()->route('topics.index')->with('success', '删除成功.');
 	}
 
 	public function uploadImage(Request $request,ImageUploadHandler $uploader){
