13f4f92343a99e4343354746ec3d73ad5b959777
TuoDi <337361454@qq.com>
2018-07-09 15:45:50 +0800

    topic delete

--- app/Models/User.php
+++ app/Models/User.php
@@ -30,4 +30,8 @@ class User extends Authenticatable
     public function topics(){
         return $this->hasMany(Topic::class,'user_id','id');
     }
+
+    public function isAuthorOf($model){
+        return $this->id == $model->user_id;
+    }
 }
