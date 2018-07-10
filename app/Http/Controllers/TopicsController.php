c8989bf6e6f44e611e6ecac2041c67fa4303bf13
TuoDi <337361454@qq.com>
2018-07-10 15:51:56 +0800

    Sidebar active users

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
 use Auth;
 use App\Handlers\ImageUploadHandler;
+use App\Models\User;
 
 class TopicsController extends Controller
 {
@@ -17,10 +18,11 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index(Request $request,Topic $topic){
+	public function index(Request $request,Topic $topic,User $user){
 		//dd($request,$topic->toSql());
 		$topics = $topic->withOrder($request->order)->paginate(18);
-		return view('topics.index', compact('topics'));
+		$active_users = $user->getActiveUsers();
+		return view('topics.index', compact('topics', 'active_users'));
 	}
 
     public function show(Topic $topic)
--- app/Models/User.php
+++ app/Models/User.php
@@ -10,6 +10,8 @@ use Spatie\Permission\Traits\HasRoles;
 class User extends Authenticatable
 {
     use HasRoles;
+
+    use Traits\ActiveUserHelper;
     
     use Notifiable{
         notify as protected laravelNotify;
@@ -58,5 +60,28 @@ class User extends Authenticatable
         $this->notification_count = 0;
         $this->save();
         $this->unreadNotifications->markAsRead();
+    }
+
+    public function setPasswordAttribute($value)
+    {
+        // 如果值的长度等于 60，即认为是已经做过加密的情况
+        if (strlen($value) != 60) {
+
+            // 不等于 60，做密码加密处理
+            $value = bcrypt($value);
+        }
+
+        $this->attributes['password'] = $value;
+    }
+
+    public function setAvatarAttribute($path)
+    {
+        // 如果不是 `http` 子串开头，那就是从后台上传的，需要补全 URL
+        if ( ! starts_with($path, 'http')) {
+            // 拼接完整的 URL
+            $path = 'http://larabbs.test/'."$path";
+        }
+
+        $this->attributes['avatar'] = $path;
     }
 }
