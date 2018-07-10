83e99db2bb858136cce55008b5f550be68ed2372
TuoDi <337361454@qq.com>
2018-07-10 16:58:08 +0800

    Setup default home page

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::get('/', 'PagesController@root')->name('root');
+Route::get('/', 'TopicsController@index')->name('root');
 
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login');
